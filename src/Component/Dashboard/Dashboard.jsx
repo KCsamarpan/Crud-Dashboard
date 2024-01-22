a58dc016d28ad9d619db3ec1cb38d86f5935f091
Your Name <you@example.com>
2024-01-22 01:07:00 -0800

    adding api

--- src/Component/Dashboard/Dashboard.jsx
+++ src/Component/Dashboard/Dashboard.jsx
@@ -1,104 +1,115 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './css/main.css'; 
-import axios from 'axios'
-import  { motion, spring } from 'framer-motion'
-
+import React, { useEffect, useState } from "react";
+import "./css/main.css";
+import axios from "axios";
+import { motion, spring } from "framer-motion";
 
 const Dashboard = () => {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const [data , setData]=useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiItems = await axios.get("https://dummyjson.com/products");
+        const response = apiItems.data.products;
+        setData(response);
+      } catch (error) {
+        console.log("error occurred", error);
+      }
+    };
+    fetchData();
+  }, []);
 
-useEffect(()=>{
-  const fetchData=async ()=>{
-    try{
-      const apiItems= await axios.get("https://dummyjson.com/products");
-      const response= apiItems.data.products;
-      setData(response);
-    }catch(error){
-      console.log("error occurred", error);
-    }
-  }
-  fetchData();
-}, []);
-  
-const handleSearch=()=>{
-  return <div>
-    <input type="text" />
-  </div>
-}
+  const filterdata = data.filter((items) =>
+    items.title.toLowerCase().includes(searchTerm.toString().toLowerCase())
+  );
 
   return (
     <div className="maindiv">
       <div className="main">
-      <motion.div className="navbar"  >
-        <motion.span 
-        whileInView={{
-          marginLeft: "-40px"
-        }}
-        transition={{
-          duration: 2,
-          stiffness: "spring"
-        }}
-        className="best">Best</motion.span>
-        <motion.span
-         whileInView={{
-          marginLeft: "40px"
-        }}
-        transition={{
-          duration: 2,
-          stiffness: "spring"
-        }}
-        
-        className="deal">Deal</motion.span>
-        <span className="tech">Tech Shop</span>
-        <div className="navbar-heading">
-          <span className="home">Home</span>
-          <span className="add">Add to cart</span>
-          <span className="home2">Home</span>
-          <div className="bar"></div>
-        </div>
-        <div className="iphonegroup"></div>
-        <motion.div
-         animate={{
-          marginTop: "580px"
-        }}
-        transition={{
-          duration: 3,
-          stiffness: spring
-        }}
-        className="applelogo"></motion.div>
-        <div className="profile"></div>
-        <div className="searchInput">
-          <input type="text" placeholder='  search...' />
-        </div>
-        <div className="search" onClick={handleSearch}></div>
- 
-      </motion.div>
-    </div>
-    {/* api export */}
-<div className="product-section">
-{
-  data.map((items , index)=> {
-    return <div key={index} className='productlist'>
-   <div className="card" style={{width: '18rem'}} key={items.id}>
-        <img src={items.images[0]}className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{items.title}</h5>
-          <p className="card-text">{items.description}</p>
-          <p className="card-text">price: {items.price}</p>
-          <a href="#" className="btn btn-primary">add to cart</a>
-        </div>
+        <motion.div className="navbar">
+          <motion.span
+            whileInView={{
+              marginLeft: "-40px",
+            }}
+            transition={{
+              duration: 2,
+              stiffness: "spring",
+            }}
+            className="best"
+          >
+            Best
+          </motion.span>
+          <motion.span
+            whileInView={{
+              marginLeft: "40px",
+            }}
+            transition={{
+              duration: 2,
+              stiffness: "spring",
+            }}
+            className="deal"
+          >
+            Deal
+          </motion.span>
+          <span className="tech">Tech Shop</span>
+          <div className="navbar-heading">
+            <span className="home">Home</span>
+            <span className="add">Products</span>
+            <div className="bar"></div>
+          </div>
+          <motion.div
+            className="iphonegroup"
+          ></motion.div>
+          <motion.div
+            animate={{
+              marginTop: "580px",
+            }}
+            transition={{
+              duration: 3,
+              type: "spring"
+            }}
+            className="applelogo"
+          ></motion.div>
+          <div className="profile"></div>
+          <div className="searchInput">
+            <input
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              type="text"
+              placeholder="  search..."
+            />
+          </div>
+          <div className="search"></div>
+        </motion.div>
       </div>
+      {/* api export */}
 
-    </div>
-  })
-}
-
-
-
-
-
-     </div>  
+      <h1 className="productheading">Product's</h1>
+      <motion.div className="product-section">
+        {filterdata.map((items, index) => {
+          return (
+            <div key={index} className="productlist">
+              <motion.div
+                whileHover={{ scale: 1.1 }}
+                className="card"
+                style={{ width: "18rem" }}
+                key={items.id}
+              >
+                <img src={items.images[0]} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{items.title}</h5>
+                  <p className="card-text">{items.description}</p>
+                  <p className="card-text">price: {items.price}</p>
+                  <a href="#" className="btn btn-primary">
+                    add to cart
+                  </a>
+                </div>
+              </motion.div>
+            </div>
+          );
+        })}
+      </motion.div>
     </div>
   );
 };
